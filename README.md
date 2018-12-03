a04b6b998ac03bd1058c31855f930e58893c0124
Donald Stolz <donaldtstolz@gmail.com>
2018-12-03 12:40:15 -0800

    Update README; Update blog-app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # amplify-practice
+
 Tutorials and projects completed to practice using the AWS Amplify Javascript library
+
+- auth: [Getting Started - React & React-native](https://aws-amplify.github.io/docs/js/react)
+- analyticsQuickStart: [Expo - React Native Authentication with Expo, AWS Amplify, & Cognito](https://www.youtube.com/watch?v=5uPNmq8TUoA)
+- blog-app: [8 steps to building your own serverless GraphQL API using AWS Amplify](https://read.acloud.guru/8-steps-to-building-your-own-serverless-graphql-api-using-aws-amplify-42c21770424d)
--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -4,16 +4,47 @@ import "./App.css";
 // import the API & graphqlOperation helpers as well as the mutation
 import { API, graphqlOperation } from "aws-amplify";
 import { createBlog, createPost, createComment } from "./graphql/mutations";
+import { getBlog } from "./graphql/queries";
 // next, we create a function to interact with the API
 
 class App extends Component {
-  state = { blogName: "" };
+  state = { blogName: "", postTitle: "", postContent: "", content: "" };
 
-  createBlog = async () => {
+  // Create a new blog
+  blog = async () => {
     const blog = { name: this.state.blogName };
     await API.graphql(graphqlOperation(createBlog, blog));
     console.log("blog successfully created");
   };
+
+  // Create a blog post
+  post = async () => {
+    const post = {
+      title: this.state.postTitle,
+      content: this.state.postContent,
+      postBlogId: this.props.blogId // Or where your blogId data lives
+    };
+    await API.graphql(graphqlOperation(createPost, post));
+    console.log("post successfully created");
+  };
+
+  // Create a comment on a blog post
+  comment = async () => {
+    const comment = {
+      content: this.state.content,
+      commentPostId: this.props.commentPostId
+    };
+    await API.graphql(graphqlOperation(createComment, comment));
+    console.log("comment successfully created");
+  };
+
+  async componentDidMount() {
+    // Get blog data
+    const data = await API.graphql(
+      graphqlOperation(getBlog, { id: "48e6bbb2-0945-4e05-8dd6-58f4b9553c64" })
+    );
+    console.log("blog successfully fetched", data);
+  }
 
   render() {
     return (
